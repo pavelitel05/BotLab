6565704e1cc1e5800d41f7075b3ce7a57ae9a4e1
pavelitel05 <pavel.stanislavovich.04@gmail.com>
2023-07-16 21:05:17 +0300

    dependencies

---
+++ src/test/java/com/pavelitelprojects/tutorbot/TutorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavelitelprojects.tutorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
