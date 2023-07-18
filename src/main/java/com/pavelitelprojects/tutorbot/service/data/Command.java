ac64f593c05540efb1e1237fd736ac77ca19063b
pavelitel05 <pavel.stanislavovich.04@gmail.com>
2023-07-19 00:27:30 +0300

    commands

---
+++ src/main/java/com/pavelitelprojects/tutorbot/service/data/Command.java
@@ -0,0 +1,7 @@
+package com.pavelitelprojects.tutorbot.service.data;
+
+public class Command {
+    public static final String START = "/start";
+    public static final String FEEDBACK = "/feedback";
+    public static final String HELP = "/help";
+}
--- src/main/java/com/pavelitelprojects/tutorbot/service/handler/CommandHandler.java
+++ src/main/java/com/pavelitelprojects/tutorbot/service/handler/CommandHandler.java
@@ -6,16 +6,64 @@ import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import static com.pavelitelprojects.tutorbot.service.data.Command.*;
+
 @Service
 public class CommandHandler {
     public BotApiMethod<?> answer(Message message, Bot bot) {
         String command = message.getText();
         switch (command) {
-            case "/start" -> {
+            case START -> {
                 return start(message);
             }
+            case FEEDBACK -> {
+                return feedback(message);
+            }
+            case HELP -> {
+                return help(message);
+            }
+            default -> {
+                return defaultAnswer(message);
+            }
         }
-        return null;
+    }
+
+    private BotApiMethod<?> defaultAnswer(Message message) {
+        return SendMessage.builder()
+                .text("Неподдерживаемая команда!")
+                .chatId(message.getChatId())
+                .build();
+    }
+
+    private BotApiMethod<?> help(Message message) {
+        return SendMessage.builder()
+                .chatId(message.getChatId())
+                .text("""
+                        📍 Доступные команды:
+                        - start
+                        - help
+                        - feedback
+                                                
+                        📍 Доступные функции:
+                        - Расписание
+                        - Домашнее задание
+                        - Контроль успеваемости
+                                                
+                        """)
+                .build();
+    }
+
+    private BotApiMethod<?> feedback(Message message) {
+        return SendMessage.builder()
+                .chatId(message.getChatId())
+                .text("""
+                        📍 Ссылки для обратной связи
+                        GitHub - https://github.com/pavelitel05
+                        LinkedIn - https://linkedin.com/in/павел-кирсанов-62b762263
+                        Telegram - https://t.me/pavelitel05
+                        """)
+                .disableWebPagePreview(true)
+                .build();
     }
 
     private BotApiMethod<?> start(Message message) {
